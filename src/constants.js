7fbf725e90625975e5fb6dcb08f77e6116fd3bfb
Raven Palte <ravenpalte@gmail.com>
2022-12-27 21:12:53 -0500

    update host constant to match dev env eb dns

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const HOST = 'https://d35oe69e4mhs3f.cloudfront.net'
-// const HOST = 'localhost'
+// export const HOST = 'https://d35oe69e4mhs3f.cloudfront.net'
+export const HOST = 'http://1973s-nft-dev-env.us-east-2.elasticbeanstalk.com'
